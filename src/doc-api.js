5ff8a852e440014ba25491c31ac4961a408cd56a
Derek Hammer <derekhammer@hotmail.com>
2018-08-30 05:38:46 -0700

    fixed api

--- src/doc-api.js
+++ src/doc-api.js
@@ -1,7 +1,10 @@
 export class Doctor {
-constructor() {}
-static showStats(name, query, print) {
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&name=${name}&skip=0&limit=10&user_key=e1d3c357c0ca979935a1290986f0f6f1`).then(function(response){
+constructor(name, query) {
+  this.name = name;
+  this.query = query;
+}
+showStats(name, query, print) {
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&name=${name}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`).then(function(response){
       print(response);
     });
 
--- src/main.js
+++ src/main.js
@@ -6,23 +6,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function(){
   $('#search').click(function(){
-    $('#query').val("");
-    $('#name').val("");
-
     let query= $('#query').val();
     let name = $('#name').val();
 
+    $('#query').val("");
+    $('#name').val("");
+
+
+    let doctor = new Doctor(name, query);
+console.log(doctor);
     function searchResults(response){
       if (response.data.length == 0) {
+        console.log(response.data);
         $(".result").text("no search results")
 
       }
 
       for (let i=0; i<response.data.length; i++){
-        $('.result').append('<p>Name:${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</p>}')
+        $('.result').append('<p>Name: '+response.data[i].profile.first_name + response.data[i].profile.last_name+ ' </p>')
 
       }
     }
-    Doctor.showStats(name, query, searchResults);
+    doctor.showStats(name, query, searchResults);
   });
 });
